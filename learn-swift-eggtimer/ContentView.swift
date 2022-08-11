3c7001c768faa1b52a397c765af4380e4f587f25
michael tamsil <michaeltamsil@gmail.com>
2022-08-12 04:45:33 +0700

    initial commit

---
+++ learn-swift-eggtimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learn-swift-eggtimer
+//
+//  Created by michael tamsil on 12/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learn-swift-eggtimer/learn_swift_eggtimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swift_eggtimerApp.swift
+//  learn-swift-eggtimer
+//
+//  Created by michael tamsil on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct learn_swift_eggtimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
